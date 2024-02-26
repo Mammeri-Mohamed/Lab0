f7c16283033059831ef18799fc450159a3f22660
mammeri <mammerim3@gamail.com>
2024-02-26 21:53:20 +0100

    commit

---
+++ EX0/src/test/java/org/example/RumanNumeralTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class RumanNumeralTest {
+
+    @Test
+    void convert() {
+    }
+
+    @Test
+    void convertModified() {
+    }
+
+    @Test
+    void testConvert() {
+    }
+
+    @Test
+    void testConvertModified() {
+    }
+}
